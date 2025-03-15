ee81891485969ff60ac467e787162af9154fb792
Carlo Contardi <c.contardi@spacecoding.it>
2025-03-15 16:51:26 +0100

    Initial Commit

---
+++ CarloCard/CarloCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarloCardApp.swift
+//  CarloCard
+//
+//  Created by Carlo Contardi on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CarloCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
